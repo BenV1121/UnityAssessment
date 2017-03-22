bdad97ac9bf80e88cbea2bd0a3cbbe7720e73b18
BenV1121 <benjamin.vongtawan@students.aie.edu.au>
2017-03-22 12:28:38 -0700

    IM THE MAP

---
+++ Assets/Scripts/EnemyDamageTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamageTrigger : MonoBehaviour
+{
+    public float damageOnContact = 5.0f;
+    public float damageOverTime = 5.0f;
+
+    private List<IDamageable> occupants = new List<IDamageable>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamageable target = other.gameObject.GetComponent<IDamageable>();
+
+        if (target == null) { return; }
+
+        target.TakeDamage(damageOnContact);
+
+        occupants.Add(target);
+    }
+
+    private void OnTriggerStat(Collider other)
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        IDamageable target = other.gameObject.GetComponent<IDamageable>();
+
+        if (target == null) { return; }
+
+        occupants.Remove(target);
+    }
+}
---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteract
+{
+    float Interaction(float inter);
+}
+
+public class Interact : MonoBehaviour
+{
+    public float interactDistance = 5f;
+
+
+    float Interaction(float inter)
+    {
+        return interactDistance;
+    }
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            interactDistance = 5f;
+
+            if (Physics.Raycast(ray, out hit, interactDistance))
+            {
+                if (hit.collider.CompareTag("Door"))
+                {
+                    hit.collider.transform.parent.GetComponent<DoorScript>().ChangeDoorState();
+                }
+            }
+        }
+    }
+}
